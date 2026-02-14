e6fd37a6cb9325e858f43d43506f30451e40af83
Dipen Bhandari <dipenbhandari205@outlook.com>
2026-02-14 17:45:03 -0600

    setup Home,Dashboard,NotFound,and CustomerPage components
    setup react routing for these pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from '../src/pages/Home';
+import Dashboard from "../src/pages/Dashboard";
+import CustomerPage from "./pages/CustomerPage";
+import NotFound from "./pages/NotFound";
+
 function App(){
   return (
-    <>
-     Waste Mangement Route Optimizer
-    </>
+    <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="customer-page" element={<CustomerPage/>}/>
+        <Route path="*" element = {<NotFound/>}/>
+       </Routes>
+    
+    
+    </BrowserRouter>
+ 
+   
   )
 }
 
