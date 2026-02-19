e607d01456eefda4d471fe17ee57896671674a9e
James Nguyen <jamesnguyen@Jamess-MacBook-Air.local>
2026-02-19 15:56:34 -0600

    parsing multiple addresses

--- backend/src/api/routes/distance.py
+++ backend/src/api/routes/distance.py
@@ -11,5 +11,17 @@ class AddressInputModel(BaseModel):
     destination: str
     
 @route.post("/addresses")
-async def get_distance_between_addresses(origin: str, destination: AddressInputModel):
-    return calculate_distance(origin, destination.destination)
+async def get_distance_between_addresses(origin: str, destinations: list[AddressInputModel]):
+    parsed_destinations = []
+    
+    for destination in destinations:
+        address_parts = [addr.strip() for addr in destination.destination.split(",")]
+        if len(address_parts) % 4 != 0:
+            raise HTTPException(status_code=400, detail=f"Invalid address format for destination: {destination.destination}. Expected format: 'street, city, state, zip'")
+        
+        for i in range(0, len(address_parts), 4):
+            parsed_address = ", ".join(address_parts[i:i+4])
+            parsed_destinations.append(parsed_address)
+        
+        
+    return calculate_distance(origin, destinations)
--- backend/src/services/distance_service.py
+++ backend/src/services/distance_service.py
@@ -1,15 +1,38 @@
 from geopy.geocoders import Nominatim
 import geopy.distance
+import requests
 
 def convert_address_to_coordinates(address):
     geolocator = Nominatim(user_agent="Waste Management App")
     location = geolocator.geocode(address)
     
-    return (location.latitude, location.longitude)
+    return [location.latitude, location.longitude]    
+    
 
-def calculate_distance(origin, destination):    
+def calculate_distance(origin, destinations):    
+    coordinates = []
     
     origin_coordinates = convert_address_to_coordinates(origin)
-    destination_coordinates = convert_address_to_coordinates(destination)
+    coordinates.append(origin_coordinates)
     
-    return geopy.distance.geodesic(origin_coordinates, destination_coordinates).miles
+    for destination in destinations:
+        destination_coordinates = convert_address_to_coordinates(destination)
+        coordinates.append(destination_coordinates)
+    
+    print(coordinates)
+    
+      
+    payload = {
+        "locations": coordinates,
+        "metrics": ["distance"],
+        "units": "miles"
+    }
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjMwZTA0NmI0YTUxZTQxOTFhN2Q0YTgyMjcxYzIzNjc5IiwiaCI6Im11cm11cjY0In0="
+    }
+    
+    url = "https://api.openrouteservice.org/v2/matrix/driving-car"
+    response = requests.post(url, json=payload, headers=headers)
+    #return geopy.distance.geodesic(origin_coordinates, destination_coordinates).miles
