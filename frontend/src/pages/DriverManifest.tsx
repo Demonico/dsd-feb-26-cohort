11de74c5bde81a4c540e1b92328229171beafe77
Dipen Bhandari <dipenbhandari205@outlook.com>
2026-02-22 10:34:28 -0600

    design manifstdetails component that stores search and details like skips,extras and stops

--- frontend/src/pages/DriverManifest.tsx
+++ frontend/src/pages/DriverManifest.tsx
@@ -1,9 +1,14 @@
 import RouteDetails from "../components/RouteDetails";
+import ManifestDetails from "@/components/ManifestDetails";
 const DriverManifest = () => {
   return (
     <div>
       <h1 className="text-xl font-bold">Driver Manifest</h1>
       <RouteDetails/>
+      <div className="border-2 p-4 rounded-sm">
+        <ManifestDetails/>
+      </div>
+      
 
      
     </div>
