e44765d9040c346fe6da7f024527ab5da8481fc9
Mandy <mandywai24@gmail.com>
2026-02-25 20:16:59 -0600

    switch location_id to UUID

--- backend/src/api/routes/servicejobs.py
+++ backend/src/api/routes/servicejobs.py
@@ -4,5 +4,5 @@ from src.services.servicejob_service import list_service_jobs_by_location
 router = APIRouter()
 
 @router.get("/service-jobs/{location_id}")
-def list_service_jobs(location_id: str):
+def list_service_jobs(location_id: UUID):
     return {"service_jobs": list_service_jobs_by_location(location_id)}
